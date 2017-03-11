c401a0635f9fb84eddf9b09976d4eab3dab0d6a4
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-03-11 11:36:07 +0100

    Add plugins and setup directories

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -4,3 +4,12 @@ dotenv.load()
 
 API_TOKEN = dotenv.get('SLACKBOT_API_TOKEN')
 DEFAULT_REPLY = "Sorry, I have not been fully configured yet."
+
+# TO DO:
+# The value for ERRORS_TO should be specified in the env file later on
+ERRORS_TO = 'bot_test'
+
+PLUGINS = [
+    'slackbot.plugins',
+    'plugins',
+]
