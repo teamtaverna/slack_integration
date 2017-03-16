629c4c0d37db6ad14df2d74c5c2de66d99b6cbd7
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-03-16 11:13:03 +0100

    Change to single quotes for consistency

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,7 @@ import dotenv
 dotenv.load()
 
 API_TOKEN = dotenv.get('SLACKBOT_API_TOKEN')
-DEFAULT_REPLY = "Sorry, I have not been fully configured yet."
+DEFAULT_REPLY = 'Sorry, I have not been fully configured yet.'
 
 # TO DO:
 # The value for ERRORS_TO should be specified in the env file later on
