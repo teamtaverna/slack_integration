8a4c0e77294a3ef5259c3a878efae87e23c1ef97
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-03-20 11:28:37 +0300

    Fix template path error

--- plugins/help.py
+++ plugins/help.py
@@ -5,14 +5,14 @@ import jinja2
 from slackbot.bot import respond_to
 
 
-def render(template_path, context=None):
+def render(template_path, context={}):
     path, filename = os.path.split(template_path)
     return jinja2.Environment(
-        loader=jinja2.FileSystemLoader(path or './')
+        loader=jinja2.FileSystemLoader(path)
     ).get_template(filename).render(context)
 
 
 @respond_to('help', re.IGNORECASE)
 def help(message):
-    response = render('../templates/help_response.j2')
+    response = render('templates/help_response.j2')
     message.reply(response)
