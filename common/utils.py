a0f5f51940f236d01546cb757596d23567c7c24c
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-03-22 12:25:07 +0300

    Refactor logic to get date for day of week and yesterday, today and tomorrow

--- common/utils.py
+++ common/utils.py
@@ -1,18 +1,53 @@
 from datetime import date, timedelta
 
 
-class TimeService:
+def get_day_arg(day_arg):
     """
-    This class handles conversion of date input from users into
+    This function takes in a day argument and returns the right timedelta
+    days argument for it.
+    For example, yesterday will be returned as -1, so that get_date function
+    can convert it to the appropriate pythonic date object.
+    We also want to be able to convert weekdays and some date format here.
+    """
+    day_arg = day_arg.lower()
+
+    day_to_num_dict = {
+        'yesterday': -1,
+        'today': 0,
+        'tomorrow': 1
+    }
+
+    day_num = day_to_num_dict.get(day_arg)
+    # More logic if a date object or weekday value is passed in.
+    if day_num is not None:
+        return day_num
+    else:
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+        if day_arg in days:
+            # In python, Monday is 0 and Sunday is 6
+            current_weekday_num = date.today().weekday()
+            day_index = days.index(day_arg)
+
+            if current_weekday_num == day_index:
+                # The weekday passed in is today
+                return day_to_num_dict['today']
+            elif current_weekday_num > day_index:
+                # It is a previous weekday
+                return 0 - (current_weekday_num - day_index)
+            else:
+                # It is a future weekday
+                return (day_index - current_weekday_num)
+        else:
+            # A date format string was passed in
+            pass
+
+
+def get_date(day_arg):
+    """
+    This function handles conversion of date input from users into
     something python can understand. For instance, when the user types
     in something like yesterday, we want to return the date of yesterday.
     """
-
-    def yesterday(self):
-        return date.today() - timedelta(days=1)
-
-    def today(self):
-        return date.today()
-
-    def tomorrow(self):
-        return date.today() + timedelta(days=1)
+    day_num = get_day_arg(day_arg)
+    return date.today() + timedelta(days=day_num)
