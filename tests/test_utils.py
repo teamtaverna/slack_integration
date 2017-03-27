c916443a1948a8fca3a904371e5f8b274d6d616f
Amos Omondi <amos.omondi@andela.com>
2017-03-27 10:43:27 +0300

    Add test for week days

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -36,3 +36,16 @@ class GetDateTest(unittest.TestCase):
         day = get_date('  today  ')
 
         self.assertEqual(day, datetime.date(2017, 3, 15))
+
+    @freeze_time("2017-03-15")
+    def test_day_of_the_week(self):
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+                'saturday', 'sunday']
+
+        self.assertEqual(get_date(days[0]), datetime.date(2017, 3, 13))
+        self.assertEqual(get_date(days[1]), datetime.date(2017, 3, 14))
+        self.assertEqual(get_date(days[2]), datetime.date(2017, 3, 15))
+        self.assertEqual(get_date(days[3]), datetime.date(2017, 3, 16))
+        self.assertEqual(get_date(days[4]), datetime.date(2017, 3, 17))
+        self.assertEqual(get_date(days[5]), datetime.date(2017, 3, 18))
+        self.assertEqual(get_date(days[6]), datetime.date(2017, 3, 19))
