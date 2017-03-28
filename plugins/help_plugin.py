27f5e7a33d0c1bcd4eba5ad30bffcceca081defc
Amos Omondi <amos.omondi@andela.com>
2017-03-28 10:52:32 +0300

    Start mocking slack message

--- plugins/help_plugin.py
+++ plugins/help_plugin.py
@@ -8,4 +8,5 @@ from common.utils import render
 @respond_to('help', re.IGNORECASE)
 def help(message):
     response = render('help_response.j2')
+    # import ipdb; ipdb.set_trace()
     message.reply(response)
--- tests/test_help_response.py
+++ tests/test_help_response.py
@@ -1,6 +1,38 @@
 import unittest
-# from plugins import help_plugin
+from plugins import help_plugin
+from unittest import mock
 
+FAKE_BOT_ID = 'US99999'
+FAKE_BOT_ATNAME = '<@' + FAKE_BOT_ID + '>'
+FAKE_CHANNEL = 'C12942JF92'
 
+class FakeClient:
+    def __init__(self):
+        self.rtm_messages = []
+
+    def rtm_send_message(self, channel, message, attachments=None):
+        self.rtm_messages.append((channel, message))
+
+class FakeMessage:
+    def __init__(self, client, msg):
+        self._client = client
+        self._msg = msg
+
+    def reply(self, message):
+        # Perhaps a bit unnecessary to do it this way, but it's close to how
+        # dispatcher and message actually works
+        self._client.rtm_send_message(self._msg['channel'], message)
+#
 class TestHelpFunction(unittest.TestCase):
-    pass
+    client = FakeClient()
+    msg = {
+        'text': FAKE_BOT_ATNAME + ' hello',
+        'channel': 'G99999'
+    }
+
+    message = FakeMessage(client, msg)
+
+
+    @mock.patch('slackbot.dispatcher.Message', return_value=message)
+    def test_help(self, mock_message):
+        self.assertIn('Help', help_plugin.help(mock_message))
