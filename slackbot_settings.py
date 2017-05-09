47c72cda340f1b0f5405b94d1228a051e0298cc5
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-05-09 23:13:16 +0300

    Remove slackbot plugins

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -5,6 +5,7 @@ from common.utils import render
 dotenv.load()
 
 API_TOKEN = dotenv.get('SLACKBOT_API_TOKEN')
+
 error_msg = "Hey there, that's a wrong command!"
 DEFAULT_REPLY = render('help_response.j2', error=error_msg)
 
@@ -13,6 +14,5 @@ DEFAULT_REPLY = render('help_response.j2', error=error_msg)
 ERRORS_TO = 'bot_test'
 
 PLUGINS = [
-    'slackbot.plugins',
-    'plugins',
+    'plugins'
 ]
