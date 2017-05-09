e7cc3ef7e760db0059975ada13e64a2fa52beccd
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-05-09 11:03:58 +0300

    Implement timetable api request

--- plugins/menu_plugin.py
+++ plugins/menu_plugin.py
@@ -1,19 +1,34 @@
 import re
+import requests
 
+import dotenv
 from slackbot.bot import respond_to
 
-from common.utils import render, get_date, get_days
+from common.utils import get_days
 
 
-def check_available_timetables():
-    pass
+dotenv.load()
+
+
+def make_api_request_for_timetables():
+    api_url = dotenv.get('API_URL_ENDPOINT')
+    query = 'query{timetables{edges{node{name, cycleLength,refCycleDay, \
+             vendors{edges{node{name}}}, admins{edges{node{username}}}}}}}'
+    endpoint = '{}?query={}'.format(api_url, query)
+    headers = {'X-TavernaToken': dotenv.get('X-TAVERNATOKEN')}
+    return requests.post(endpoint, headers=headers).json()['timetables']
+
+
+def check_num_available_timetables():
+    timetables = make_api_request_for_timetables()
+    return len(timetables)
+
 
 @respond_to('menu', re.IGNORECASE)
 def menu(message):
     days = get_days()
 
     if message.body['text'] == 'menu':
-        timetables = check_available_timetables()
         # TODO: if timetables == 1, return menu for the day
         # else
         # let user know the list of timetables available
