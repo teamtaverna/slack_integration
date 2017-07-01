be46ba524c1ace830753ccfe084404bb011b14a1
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-07-01 19:45:21 +0100

    Update help commands

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -6,10 +6,6 @@ dotenv.load()
 
 API_TOKEN = dotenv.get('SLACKBOT_API_TOKEN')
 
-# TO FIX: error message showing in help.
-# error_msg = "Hey there, that's a wrong command!"
-# DEFAULT_REPLY = render('help_response.j2', error=error_msg)
-
 DEFAULT_REPLY = render('help_response.j2')
 
 # TO DO:
