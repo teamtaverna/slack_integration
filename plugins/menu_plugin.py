4305979cc629a7cbb0eb0ab4544fa5374c7de001
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-07-01 19:22:31 +0100

    Render menu with single timetable

--- plugins/menu_plugin.py
+++ plugins/menu_plugin.py
@@ -1,4 +1,5 @@
 import re
+from operator import itemgetter
 
 from slackbot.bot import respond_to
 
@@ -12,15 +13,41 @@ def make_api_request_for_timetables():
 
 
 def make_api_request_for_servings(timetable, date):
-    query = 'query{servings(timetable:"%s",date:"%s"){dateServed,\
+    query = 'query{servings(timetable:"%s",date:"%s"){publicId, dateServed, vendor{name},\
              menuItem{cycleDay,meal{name},course{name,sequenceOrder},\
-             dish{name},timetable{name}}}}'% (timetable, date)
+             dish{name},timetable{name}}}}' % (timetable, date)
     return make_api_request(query)['servings']
 
 
 def list_timetable_names():
     timetables = make_api_request_for_timetables()
     return [timetable['slug'] for timetable in timetables]
+
+
+def servings_to_dict(servings):
+    new_dict = {}
+
+    for obj in servings:
+        meal = obj['menuItem']['meal']['name']
+        menu_item = obj['menuItem']
+        new_obj = {
+            'public_id': obj['publicId'],
+            'course': menu_item['course']['name'],
+            'sequence_order': menu_item['course']['sequenceOrder'],
+            'dish': menu_item['dish']['name']
+        }
+        if new_dict.get(meal):
+            new_dict[meal].append(new_obj)
+        else:
+            new_dict[meal] = [new_obj]
+
+    # Re-order the meals by sequence order
+    return {
+        key: sorted(
+            value,
+            key=itemgetter('sequence_order')
+        ) for key, value in new_dict.items()
+    }
 
 
 @respond_to('menu', re.IGNORECASE)
@@ -35,15 +62,19 @@ def menu(message):
 
     if message_text == 'menu':
         num_timetables = len(timetable_names)
-        servings = make_api_request_for_servings(timetable_names[0], date_to_str('today'))
-        print(servings)
-        if num_timetables == 1:
-            pass
+        servings = make_api_request_for_servings(
+            timetable_names[0], date_to_str('today')
+        )
 
         context = {
             'num_of_timetables': num_timetables,
             'timetable_names': timetable_names
         }
+
+        if num_timetables == 1 and servings is not None:
+            meals = servings_to_dict(servings)
+            context.update({'meals': meals})
+
         response = render('menu_response.j2', context)
         message.reply(response)
     elif len_msg_text_list == 2 and message_text_list[1] in timetable_names:
